f3d87d4676fc40287736f497f935e5c0aeac1609
Soyeb <tssoyeb@gmail.com>
2022-04-28 15:47:12 +0600

    create server site,connect with mongodb and implement data get from db

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qfyfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('EmaJohn').collection('product')
+
+        app.get('/product', async (req, res) => {
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size)
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if (page || size) {
+                products = await cursor.skip(page*size).toArray();
+            } else {
+                products = await cursor.toArray();
+            }
+
+            res.send(products)
+        })
+
+        app.get('/productCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('John is running and  waiting for data')
+})
+app.listen(port, () => {
+    console.log('john is running on port : ', port);
+})
